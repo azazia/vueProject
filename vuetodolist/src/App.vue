9c33619714556c48c351d3d3f7ef11927438b7fd
azazia <azaziahmad9@gmail.com>
2022-01-04 23:26:02 +0800

    calc

--- vuetodolist/src/App.vue
+++ vuetodolist/src/App.vue
@@ -1,19 +1,30 @@
 <template>
     <div id="app">
-        <h2>
-            What I do today
-        </h2>
-        <div class="pekerjaan-rumah">
-            <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="tugas">
-                <p>{{tugasSaya.namaTugas}}</p>
+        <div class="container">
+            <div class="row header p-5">
+                <div class="col-lg-12">
+                    <h2>What I do today</h2>
+                </div>
             </div>
-            
+            <div class="row content">
+                <div class="col-lg-12">
+                    <div class="pekerjaan-rumah">
+                        <div class="row">
+                            <div v-for="tugasSaya in tugas" v-bind:key="tugasSaya.id" class="mb-3 tugas col-lg-12 text-left">
+                                <ItemTugas v-bind:tugasSaya="tugasSaya"/>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 
+import 'bootstrap/dist/css/bootstrap.css'
+import ItemTugas from './components/ItemTugas.vue'
 
 export default {
   name: 'App',
@@ -21,22 +32,25 @@ export default {
       return{
         tugas: [
             {
-                  id: 1,
-                namaTugas: "Menyapu Rumah"
+                id: 1,
+                namaTugas: "Menyapu Rumah",
+                isDone: false
             },
             {
                 id: 2,
-                namaTugas: "Memasak ayam"
+                namaTugas: "Memasak ayam",
+                isDone: false
             },
             {
                 id: 3,
-                namaTugas: "Belajar pemogramman"
+                namaTugas: "Belajar pemogramman",
+                isDone: false
             }
         ]
       }
   },
   components: {
-    
+    ItemTugas
   }
 }
 </script>
@@ -49,5 +63,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+    background: blue;
+    color: white;
+    margin-bottom: 20px;
+}
+.header h2{
+    margin: 0px;
+}
+.tugas {
+    font-size: 20px;
+}
+
+.item{
+    color: green;
 }
 </style>
